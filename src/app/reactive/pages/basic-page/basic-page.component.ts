b7547600a582c47dcc7bac936f8fa0184b01138a
Raúl Morales <raulmdaw@gmail.com>
2024-01-24 23:55:31 +0100

    240. Métodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -9,7 +9,7 @@ const rxt4090 = {
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
@@ -19,24 +19,47 @@ export class BasicPageComponent implements OnInit {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ] ],
-    price: [0, [ Validators.required, Validators.min(0) ] ],
-    inStorage: [0, [ Validators.required, Validators.min(0) ] ],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  constructor( private fb: FormBuilder ) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     // this.myForm.reset(rxt4090);
   }
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors &&
+      this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracteres`;
+      }
+    }
+
+    return null;
+  }
+
   onSave(): void {
-    if ( this.myForm.invalid ) {
+    if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
       return;
     }
     console.log(this.myForm.value);
 
-    this.myForm.reset({price: 0, inStorage: 0});
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
