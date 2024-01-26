03ae8c118899e723fad3cae357c07780e631dc8b
Raúl Morales <raulmdaw@gmail.com>
2024-01-26 21:39:05 +0100

    256. Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -7,13 +7,35 @@ export class EmailValidatorService implements AsyncValidator {
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log(email);
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber) => {
+      console.log({email});
+
+      // el email introducido es válido (no se está usando actualmente)
+      if (email === 'raul@test.es') {
+        subscriber.next({emailTaken: true});
+        subscriber.complete();
+      }
+
+      // el email introducido no existe
+      subscriber.next(null);
+      subscriber.complete();
     }).pipe(
-      delay(2000)
-    )
+      delay(3000)
+    );
+
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log(email);
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+  // }
 
 }
