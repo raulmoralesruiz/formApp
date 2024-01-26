2a004a5ed56bd5b5f8ab40cc4e6ed4c2b6e75ab6
Raúl Morales <raulmdaw@gmail.com>
2024-01-26 21:27:15 +0100

    255. Validadores asíncronos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,27 +1,42 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern) ]],
-    email: ['', [ Validators.required, Validators.pattern(this.validatorsService.emailPattern) ]],
-    username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
-    password: ['', [ Validators.required, Validators.minLength(6) ]],
-    password2: ['', [ Validators.required ]],
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.validatorsService.emailPattern),
+      ],
+      [ this.emailValidatorService ],
+    ],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
   });
 
-  constructor (
+  constructor(
     private fb: FormBuilder,
     private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService,
   ) {}
 
-  isValidField( field: string) {
-    return this.validatorsService.isValidField( this.myForm, field );
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
